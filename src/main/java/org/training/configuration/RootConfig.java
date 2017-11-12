c67bb9e6aeaf761b125a8e71e456839b642bc272
andrewyashin <razzer9876@gmail.com>
2017-11-13 00:32:34 +0200

    Modify root config

--- src/main/java/org/training/configuration/RootConfig.java
+++ src/main/java/org/training/configuration/RootConfig.java
@@ -1,4 +1,16 @@
 package org.training.configuration;
 
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan(value = "org.training",
+        excludeFilters = {
+            @ComponentScan.Filter(type = FilterType.ANNOTATION, value = EnableWebMvc.class)
+        }
+)
 public class RootConfig {
 }
